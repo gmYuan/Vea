b3bb7a416a6d15fc0648df497e2cf38ff4b3ef68
gmYuan <ygm121351@163.com>
2019-10-08 20:28:19 +0800

    初始化项目为单文件组件 + 引入parcel

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Vea
 
 Vue造轮子系列
+
+
+## 安装
+
+使用本框架前，请在 CSS中设置盒模型为 border-box
+
+```css
+  * {box-sizing: border-box;}
+```
---
+++ src/button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="ea-button">按钮</button>
+</template>
+
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang="scss">
+  .ea-button {
+    height: var(--button-height);
+    font-size: var(--font-size);
+    padding: 0 1em;
+    background: var(--button-bg);
+
+    border: 1px solid var(--border-color);
+    border-radius: var(--border-radius);
+
+
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+</style>
