881bb6e78e6ffa17bbcbdda4a56d4fd6ab39094a
gmYuan <ygm121351@163.com>
2019-10-11 21:19:28 +0800

    对buttonGroup进行限制

--- src/button-group.vue
+++ src/button-group.vue
@@ -8,7 +8,24 @@
 
 
 <script>
-  export default {}
+  export default {
+
+    methods: {
+      checkEleType() {
+        for (let ele of this.$el.children) {  // ele是DOM元素
+
+          let eleName = ele.nodeName.toLowerCase()
+          if (eleName !== 'button') {
+            console.warn(`e-button-group只能使用e-button, 而你使用的元素是${eleName}`)
+          }
+        }
+      }
+    },
+
+    mounted() {
+      this.checkEleType()
+    }
+  }
 </script>
 
 <style scoped lang="scss">
@@ -17,25 +34,26 @@
     display: inline-flex;
     vertical-align: middle;
 
-    .e-button {    /* 仅首尾按钮是 圆角 + 隐藏重合border边框*/
+    .e-button { /* 仅首尾按钮是 圆角 + 隐藏重合border边框*/
       border-radius: 0;
       margin-left: -1px;
-      &:hover {   /* hover时显示 隐藏的重合border边框*/
+
+      &:hover { /* hover时显示 隐藏的重合border边框*/
         position: relative;
         z-index: 1;
       }
-      
+
       &:first-child {
         border-top-left-radius: var(--border-radius);
         border-bottom-left-radius: var(--border-radius);
       }
+
       &:last-child {
         border-top-right-radius: var(--border-radius);
         border-bottom-right-radius: var(--border-radius);
       }
 
     }
-
 
 
   }
